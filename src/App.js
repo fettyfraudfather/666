8297cd52a5fa26e02a4eeadf2eb4d50ac2383746
fettyfraudfather <86132501+fettyfraudfather@users.noreply.github.com>
2021-07-09 18:43:41 -0400

    Initialize reactjs

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import './App.css';
+import Experience from './pages/Experience/Experience'
+import Projects from './pages/Projects/Projects'
+import Research from './pages/Research/Research'
+import Navbar from './components/Navbar/Navbar';
+import Education from './pages/Education/Education'
+import Resume from './pages/Resume/Resume';
+import NotFound from './pages/404/404';
+import Footer from './components/Footer/Footer';
+
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/experience' component={Experience} />
+          <Route path='/projects' component={Projects} />
+          <Route path='/education' component={Education} />
+          <Route path='/research' component={Research} />
+          <Route path='' component={NotFound} />
+          <Route path='/resume' component={Resume} />
+        </Switch>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
